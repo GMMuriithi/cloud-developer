17a8a718826c92369ddd19bc1e56de92b8b204f0
Mark Muriithi <mcgithogori@gmail.com>
2022-07-08 19:11:12 +0300

    Commits to dev repo

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -28,7 +28,32 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get("/filteredimage", async (req, res) => {
 
+    let {image_url} = req.query;
+
+    // validate the image_url query
+    if(!image_url){
+      return res.status(400).send(`image_url is required!`);
+
+    }
+    //call filterImageFromURL(image_url) to filter the image
+    filterImageFromURL(image_url).then(image => { 
+      
+      // send the resulting file in the response
+      res.status(200).sendFile(image, function (err) {
+        if(!err){
+          let files: Array<string> = [image];
+          //deletes any files on the server on finish of the response
+          deleteLocalFiles(files);
+        }
+      });
+    
+      }).catch(err => {
+        res.status(422).send(`${err}`);
+      });
+
+  });
   //! END @TODO1
   
   // Root Endpoint
@@ -36,6 +61,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
+  
   
 
   // Start the Server
